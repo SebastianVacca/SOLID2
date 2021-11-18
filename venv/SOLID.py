4fb4cebc22bda9f80f6c64fac5c9fa4b6031c503
SebastianVacca <jvaccape@ibero.edu.co>
2021-11-18 17:45:55 -0500

    encontrat manera de transcribir a python el codigo con el principo open/close

---
+++ venv/SOLID.py
@@ -0,0 +1,20 @@
+class coche:
+    marca = ""
+
+    def __init__(self, marca):
+         self.marca = ""
+
+    def getmarca(self):
+        return marca
+
+    def getmarcaCoche(string):
+        return marca
+
+
+class cocheDB:
+
+    def guardarCocheDB(Coche):
+        {""}
+    def eliminarCocheDB(Coche):
+        {""}
+
---
+++ venv/SOLID2.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class Coche(ABC):
+    @abstractmethod
+    def precioMedioCoche(self):
+        pass
+
+class Renault 'extends' Coche:
+    'override'
+    precioMedioCoche()
+        return "18000"
+
+class Auidi 'extends' Coche:
+    'override'
+    precioMedioCoche()
+        return "25000"
+
+class Mercedes 'extends' Coche:
+    'override'
+    precioMedioCoche()
+        return "27000"
+
