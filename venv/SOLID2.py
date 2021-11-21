90bffd78542c76e9a9563a7d348d91b0d5071bf7
SebastianVacca <jvaccape@ibero.edu.co>
2021-11-20 20:23:34 -0500

    se define el metodo imprimirNumAsientos(Coches) se demuestra el principio de sustitucion de Liskov

--- venv/SOLID2.py
+++ venv/SOLID2.py
@@ -4,6 +4,8 @@ class Coche(ABC):
     @abstractmethod
     def precioMedioCoche(self):
         pass
+    def numAsientos(self):
+        pass
 
 class Renault (Coche):
     def precioMedioCoche(self):
@@ -13,10 +15,14 @@ class Renault (Coche):
 class Auidi (Coche):
     def precioMedioCoche(self):
         return "25000"
+    def numAsientos(self):
+        return "2"
 
 class Mercedes(Coche):
     def precioMedioCoche(self):
         return "27000"
+    def numAsientos(self):
+        return "2"
 
 
 def imprimirPrecioMedioCoche(Coche):
@@ -24,7 +30,22 @@ def imprimirPrecioMedioCoche(Coche):
     for i in Coche:
         print(i.precioMedioCoche())
 
+def imprimirNumAsientos(Coche):
+
+    for i in Coche:
+        if Coche is Renault:
+            print(numAsientos(Coche))
+        if Coche is Audi:
+            print(numAsientos(Coche))
+        if Coche is Mercedes:
+            print(numAsientos(Coche))
+
+def imprimirNumAsientos(Coche):
+    for i in Coche:
+        print(i.numAsientos())
+
 if __name__ == '__main__':
 
     coches = [Renault(), Auidi(), Mercedes()]
     imprimirPrecioMedioCoche(coches)
+    imprimirNumAsientos(coches)
